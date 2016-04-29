dc936a06eab0c908a381cea8cf9653ce3ed95e30
Juan Rada <jrada@pros.com>
2016-04-29 01:54:16 +0100

    added variable frames configuration support

--- InfoReader/src/FileReader.groovy
+++ InfoReader/src/FileReader.groovy
@@ -6,18 +6,19 @@ package reader
 
 class FileReader
 {
-    static final VIDEOS = ["PartyScene", "ParkRun", "Kimono1", "BQMall", "City", "PartyScene", "Traffic"]
+    static final int[] FRAMES = [128, 128, 128, 128, 128, 128, 128, 97, 97]
+    static final VIDEOS = ["PartyScene", "ParkRun", "Kimono1", "BQMall", "City", "PartyScene", "Traffic", "PeopleOnStreet", "BasketballDrive"]
     static final Resoulutions = ["intra", "ip_8", "ip_16", "ip_32", "ip_64"]
 
-    static ProcessInfo readFile(String videoName, String resolution)
+    static ProcessInfo readFile(String videoName, String resolution, int frames)
     {
         String fileName = '../../HEVC_TEST/' + videoName + '/' + videoName + '_frames128_' + resolution + '.out'
         File file = new File(fileName)
         String[] lines = file.readLines()
 
-        def iInfo = lines[203].replaceAll("\\s+", " ").split(" ")
-        def bInfo = lines[213].replaceAll("\\s+", " ").split(" ")
-        def info = lines[198].replaceAll("\\s+", " ").split(" ")
+        def info = lines[frames + 70].replaceAll("\\s+", " ").split(" ")
+        def iInfo = lines[frames + 75].replaceAll("\\s+", " ").split(" ")
+        def bInfo = lines[frames + 85].replaceAll("\\s+", " ").split(" ")
 
         String i_frames = iInfo[1]
         String i_bitrate = iInfo[3]
@@ -31,7 +32,7 @@ class FileReader
         String bitrate = info[3]
         String psnr = info[7]
 
-        String totalTime = lines[218].split(" ")[6]
+        String totalTime = lines[frames + 90].split(" ")[6]
 
         new ProcessInfo(
             video: videoName,
@@ -50,10 +51,12 @@ class FileReader
 
     static void main(String[] args)
     {
+        int i = 0;
         def processData = []
         VIDEOS.each { video ->
+            int frames = FRAMES[i++];
             Resoulutions.each { res ->
-                processData.add(readFile(video, res))
+                processData.add(readFile(video, res, frames))
             }
         }
 
